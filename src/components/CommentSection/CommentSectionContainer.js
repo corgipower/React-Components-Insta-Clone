ae9b4ba069550093950fe169d7971a583841a21f
Liz Shulman <corgipower@hotmail.com>
2020-06-20 13:57:23 -0400

    finished commentsectioncontainer, i think

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,11 +6,24 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments, setComments] = useState(props.comments);
+  console.log('CommentSection', comments);
+  const submitComment = (e, txt) => {
+    e.preventDefault();
+    let newComment = {
+      username: 'Liz',
+      text: txt
+    }
+    setComments([...comments, newComment]);
+  }
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comments.map(c => (
+        <Comment key={c.username} comment={c} />
+      ))}
+      <CommentInput submitComment={submitComment} comments={comments} />
     </div>
   );
 };
